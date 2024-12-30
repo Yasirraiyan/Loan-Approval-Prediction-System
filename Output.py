5b110126b83444a31e6edcb67bc39a38207ce2ad
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-30 13:05:27 +0600

    Create Output.py

---
+++ Output.py
@@ -0,0 +1,26 @@
+ApplicantIncome  LoanAmount  CreditHistory  Approved
+0             5000         100              1         1
+1             6000         150              0         0
+2             7000         200              1         1
+3             8000         250              1         1
+4             9000         300              0         0
+5            10000         350              1         1
+6            11000         400              0         0
+7            12000         450              1         1
+8            13000         500              1         1
+9            14000         550              0         0
+Accuracy: 100.00%
+Classification Report:
+              precision    recall  f1-score   support
+
+           0       1.00      1.00      1.00         1
+           1       1.00      1.00      1.00         1
+
+    accuracy                           1.00         2
+   macro avg       1.00      1.00      1.00         2
+weighted avg       1.00      1.00      1.00         2
+
+Confusion Matrix:
+[[1 0]
+ [0 1]]
+Loan Approval Status: Approved
